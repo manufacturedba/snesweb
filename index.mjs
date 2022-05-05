03f52961291a4dfa92f2e041275a7cc4f36fed29
Roberto Rodriguez <manufacturedba@gmail.com>
2022-05-05 00:14:07 -0700

    Correct indentation per eslint

--- index.mjs
+++ index.mjs
@@ -8,18 +8,17 @@ const dirName = path.dirname(fileName);
 const PORT = process.env.PORT || 616;
 
 (async () => {
+  const server = hapi.server({
+    port: PORT,
+    host: '0.0.0.0',
+    routes: {
+      files: {
+        relativeTo: path.join(dirName, 'dist'),
+      },
+    },
+  });
 
-    const server = hapi.server({
-        port: PORT,
-        host: '0.0.0.0',
-        routes: {
-            files: {
-                relativeTo: path.join(dirName, 'dist')
-            }
-        }
-    });
+  await server.start();
 
-    await server.start();
-
-    console.log('Tepache Web running at', PORT);
+  console.log('Tepache Web running at', PORT);
 })();
