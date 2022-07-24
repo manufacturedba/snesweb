7d6daf3268fc22b23d642d7b6a13d0dbd51c3995
Roberto Rodriguez <manufacturedba@gmail.com>
2022-07-24 16:43:22 -0700

    Add db seeding script

---
+++ scripts/seed-firestore.mjs
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker';
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore, Timestamp } from 'firebase-admin/firestore';
+import process from 'node:process';
+
+const projectId = 'tepache-mode';
+process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
+initializeApp({ projectId });
+
+const db = getFirestore();
+
+(async () => {
+  try {
+    await db.collection('tepacheGames').add({
+      urn: `urn:tepache-game:${faker.random.numeric(5)}`,
+      title: faker.lorem.word(),
+      description: faker.lorem.sentence(),
+      active: faker.random.boolean(),
+      createdAt: Timestamp.now(),
+      logo: faker.internet.avatar(),
+      playModes: ['TEAM', 'ADMIN_CONTROL'],
+    });
+
+    console.log('Successfully seeded firestore emulation');
+  } catch (error) {
+    console.error(error, 'Unable to seed firestore emulation');
+  }
+})();
