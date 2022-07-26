a17c93258efda2ef5a12adcebb2716d818b2b62e
Roberto Rodriguez <manufacturedba@gmail.com>
2022-07-25 19:56:37 -0700

    Move authentication

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,11 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { getAnalytics } from 'firebase/analytics';
+import {
+  getAuth,
+  signInAnonymously,
+} from 'ember-cloud-firestore-adapter/firebase/auth';
+import { Promise } from 'rsvp';
 
 export default class ApplicationRoute extends Route {
   @service
@@ -8,6 +13,33 @@ export default class ApplicationRoute extends Route {
 
   @service('-firebase')
   firebase;
+
+  @service
+  session;
+
+  model() {
+    return new Promise((resolve, reject) => {
+      const auth = this.firebase.auth();
+
+      const isNotAuthenticated = this.session.prohibitAuthentication(() =>
+        resolve(auth.currentUser)
+      );
+
+      if (isNotAuthenticated) {
+        return signInAnonymously(auth)
+          .then(() => resolve(auth.currentUser))
+          .catch((error) => {
+            const { code: errorCode } = error;
+
+            if (errorCode === 'auth/operation-not-allowed') {
+              reject('You must enable Anonymous auth in the Firebase Console.');
+            } else {
+              reject(error);
+            }
+          });
+      }
+    });
+  }
 
   beforeModel() {
     getAnalytics();
--- app/routes/base.js
+++ app/routes/base.js
@@ -1,48 +1,12 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import {
-  getAuth,
-  signInAnonymously,
-} from 'ember-cloud-firestore-adapter/firebase/auth';
-import { Promise } from 'rsvp';
 
 export default class BaseRoute extends Route {
-  @service
-  store;
-
   @service
   router;
 
   @service
-  session;
-
-  @service
   remoteConfig;
-
-  model() {
-    return new Promise((resolve, reject) => {
-      const auth = getAuth();
-
-      const isNotAuthenticated = this.session.prohibitAuthentication(() =>
-        resolve(auth.currentUser)
-      );
-
-      if (isNotAuthenticated) {
-        const auth = getAuth();
-        return signInAnonymously(auth)
-          .then(() => resolve(auth.currentUser))
-          .catch((error) => {
-            const { code: errorCode } = error;
-
-            if (errorCode === 'auth/operation-not-allowed') {
-              reject('You must enable Anonymous auth in the Firebase Console.');
-            } else {
-              reject(error);
-            }
-          });
-      }
-    });
-  }
 
   beforeModel() {
     const live = this.remoteConfig.getBoolean('live');
