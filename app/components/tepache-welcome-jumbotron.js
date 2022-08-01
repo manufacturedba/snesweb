70ea3e64b23a40397a5995f0430e368e0891b6a1
Roberto Rodriguez <manufacturedba@gmail.com>
2022-08-01 01:20:43 -0700

    Allow play once game is set

--- app/components/tepache-welcome-jumbotron.js
+++ app/components/tepache-welcome-jumbotron.js
@@ -1,3 +1,13 @@
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default class TepacheWelcomeJumbotronComponent extends Component {}
+export default class TepacheWelcomeJumbotronComponent extends Component {
+  @service
+  router;
+
+  @action
+  transitionToMagwest() {
+    this.router.transitionTo('authenticated.base.magwest');
+  }
+}
--- app/routes/authenticated/base/index.js
+++ app/routes/authenticated/base/index.js
@@ -1,9 +1,37 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import {
+  query,
+  where,
+  limit,
+} from 'ember-cloud-firestore-adapter/firebase/firestore';
 
 export default class BaseIndexRoute extends Route {
   @service
   store;
 
-  model() {}
+  @service
+  remoteConfig;
+
+  async model() {
+    const magWestUrn = this.remoteConfig.getString('magwest_urn');
+    const live = this.remoteConfig.getBoolean('live');
+
+    if (live && magWestUrn) {
+      const games = await this.store.query('tepache-game', {
+        queryId: magWestUrn,
+
+        filter(reference) {
+          return query(
+            reference,
+            where('active', '==', true),
+            where('urn', '==', magWestUrn),
+            limit(1)
+          );
+        },
+      });
+
+      return games.firstObject;
+    }
+  }
 }
