0486fcbd04e3d366d37d9972131ec45f6df4f5a2
Roberto Rodriguez <manufacturedba@gmail.com>
2022-08-20 01:59:48 -0700

    Controller API
    Improvements to handling socket errors

--- app/components/tepache-game-client-controller.js
+++ app/components/tepache-game-client-controller.js
@@ -1,14 +1,64 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 const BUTTONS_FOR_HIDE_TOGGLE = ['left', 'right', 'up', 'down'];
 
 const dataAttribute = 'data-tepache-game-client-controller-target';
 
+const NOT_SUPPORTED = 'NOT_SUPPORTED';
+
+const buttonMap = {
+  0: 'b',
+  1: 'a',
+  2: 'x',
+  3: 'y',
+  4: 'l',
+  5: 'r',
+  6: NOT_SUPPORTED,
+  7: NOT_SUPPORTED,
+  8: 'select',
+  9: 'start',
+  10: NOT_SUPPORTED,
+  11: NOT_SUPPORTED,
+  12: 'up',
+  13: 'down',
+  14: 'left',
+  15: 'right',
+  16: NOT_SUPPORTED,
+};
+
+const throttleTime = 200; // ms
+
 export default class TepacheGameClientControllerComponent extends Component {
   @service
   nes;
+
+  @tracked
+  socketConnected = true;
+
+  #connectUnsubscribe;
+
+  #disconnectUnsubscribe;
+
+  #errorUnsubscribe;
+
+  constructor() {
+    super(...arguments);
+
+    this.#connectUnsubscribe = this.nes.onConnect(() => {
+      this.socketConnected = true;
+    });
+
+    this.#disconnectUnsubscribe = this.nes.onDisconnect(() => {
+      this.socketConnected = false;
+    });
+
+    this.#errorUnsubscribe = this.nes.onError(() => {
+      this.socketConnected = false;
+    });
+  }
 
   @action
   /**
@@ -60,5 +110,62 @@ export default class TepacheGameClientControllerComponent extends Component {
         )
         .classList.remove('visible');
     });
+  }
+
+  @action
+  listenForController() {
+    let lastTime = 0;
+    let lastButton;
+
+    let activeGamepadIndex;
+    let activeGamepad;
+
+    const update = () => {
+      const gamepads = navigator.getGamepads();
+      const gameIndex = gamepads.findIndex(
+        (gamepad) => gamepad && gamepad.buttons.find((button) => button.pressed)
+      );
+
+      activeGamepadIndex = gameIndex !== -1 ? gameIndex : activeGamepadIndex;
+      activeGamepad = gamepads[activeGamepadIndex];
+
+      if (activeGamepad && activeGamepad.buttons.length) {
+        Object.keys(buttonMap).forEach((key) => {
+          const activeGamepadButton = activeGamepad.buttons[key];
+          if (buttonMap[key] !== NOT_SUPPORTED) {
+            if (activeGamepadButton.pressed) {
+              const now = new Date().getTime();
+
+              if (lastButton !== key || now - throttleTime >= lastTime) {
+                lastTime = now;
+                lastButton = key;
+
+                this.nes.request({
+                  path: '/api/socket/tepache-session-captures',
+                  method: 'POST',
+                  payload: {
+                    button: buttonMap[key],
+                    gameSessionUrn: this.args.gameSessionModel.urn,
+                  },
+                });
+              }
+            }
+          }
+        });
+      }
+
+      window.requestAnimationFrame(update);
+    };
+
+    window.requestAnimationFrame(update);
+  }
+
+  @action
+  remove() {
+    super.willDestroy(...arguments);
+
+    this.#connectUnsubscribe();
+    this.#errorUnsubscribe();
+    this.#disconnectUnsubscribe();
   }
 }
--- app/services/nes.js
+++ app/services/nes.js
@@ -12,11 +12,18 @@ export default class NesService extends Service {
 
   #connectionRequest;
 
+  #callbacks = {};
+
   constructor() {
     super(...arguments);
     const host = config['hapi-nes'].host;
 
     assert('hapi-nes host is required', host);
+
+    this.#callbacks.onConnect = [];
+    this.#callbacks.onDisconnect = [];
+    this.#callbacks.onHeartbeatTimeout = [];
+    this.#callbacks.onError = [];
 
     this.#client = new Client(host);
 
@@ -27,6 +34,26 @@ export default class NesService extends Service {
         },
       },
     });
+
+    this.#client.onConnect = () => {
+      this.#callbacks.onConnect.forEach((callback) => callback());
+    };
+
+    this.#client.onDisconnect = (willReconnect, log) => {
+      this.#callbacks.onDisconnect.forEach((callback) =>
+        callback(willReconnect, log)
+      );
+    };
+
+    this.#client.onHeartbeatTimeout = (willReconnect) => {
+      this.#callbacks.onHeartbeatTimeout.forEach((callback) =>
+        callback(willReconnect)
+      );
+    };
+
+    this.#client.onError = (err) => {
+      this.#callbacks.onError.forEach((callback) => callback(err));
+    };
   }
 
   async request(path, payload) {
@@ -36,5 +63,49 @@ export default class NesService extends Service {
 
   get connected() {
     return this.#client.id;
+  }
+
+  onConnect(callback) {
+    this.#callbacks.onConnect.push(callback);
+    return () => {
+      this.#callbacks.onConnect.forEach((cb, index) => {
+        if (cb === callback) {
+          this.#callbacks.onConnect.splice(index, 1);
+        }
+      });
+    };
+  }
+
+  onDisconnect(callback) {
+    this.#callbacks.onDisconnect.push(callback);
+    return () => {
+      this.#callbacks.onDisconnect.forEach((cb, index) => {
+        if (cb === callback) {
+          this.#callbacks.onDisconnect.splice(index, 1);
+        }
+      });
+    };
+  }
+
+  onHeartbeatTimeout(callback) {
+    this.#callbacks.onHeartbeatTimeout.push(callback);
+    return () => {
+      this.#callbacks.onHeartbeatTimeout.forEach((cb, index) => {
+        if (cb === callback) {
+          this.#callbacks.onHeartbeatTimeout.splice(index, 1);
+        }
+      });
+    };
+  }
+
+  onError(callback) {
+    this.#callbacks.onError.push(callback);
+    return () => {
+      this.#callbacks.onError.forEach((cb, index) => {
+        if (cb === callback) {
+          this.#callbacks.onError.splice(index, 1);
+        }
+      });
+    };
   }
 }
