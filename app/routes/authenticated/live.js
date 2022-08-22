2ee68c4ed84ed7faa01eb369b4733ff76270b0f3
Roberto Rodriguez <manufacturedba@gmail.com>
2022-08-22 09:32:44 -0700

    Up limit for listener

--- app/routes/authenticated/live.js
+++ app/routes/authenticated/live.js
@@ -90,7 +90,7 @@ export default class MagwestLiveRoute extends Route {
           reference,
           where('createdAt', '>', lastHour),
           orderBy('createdAt', 'desc'),
-          limit(9)
+          limit(10)
         );
       },
     });
@@ -106,7 +106,7 @@ export default class MagwestLiveRoute extends Route {
           where('playerSessionUrn', '==', playerSession.urn),
           where('createdAt', '>', lastHour),
           orderBy('createdAt', 'desc'),
-          limit(1)
+          limit(3)
         );
       },
     });
