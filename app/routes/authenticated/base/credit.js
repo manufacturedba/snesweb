4626a97703821901931d3da4724107242baba86b
Roberto Rodriguez <roberto@thinkcrunch.com>
2022-08-22 00:30:36 -0700

    real sorr

--- app/routes/authenticated/base/credit.js
+++ app/routes/authenticated/base/credit.js
@@ -11,7 +11,7 @@ export default class CreditRoute extends Route {
           "Alec Asperslag makes art and sometimes other things. A piece of Alec's soul was once locked into a puppet (is it still there?). Alec lacks the ability to self-describe. Find Alec on twitter or ArtStation.",
         links: [
           'https://mobile.twitter.com/asperslaga',
-          'https://www.artstation.com/alecasperslag',
+          'https://www.artstation.com/alecasperslaga',
         ],
       },
       {
