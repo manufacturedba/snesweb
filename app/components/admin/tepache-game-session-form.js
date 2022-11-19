a7d468af64847be7548aa36a2077fe026c5a8344
Roberto Rodriguez <manufacturedba@gmail.com>
2022-11-18 23:48:07 -0800

    Permit saving of game session model

--- app/components/admin/tepache-game-session-form.js
+++ app/components/admin/tepache-game-session-form.js
@@ -1,6 +1,17 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { GAME_SESSION_STATE } from 'tepacheweb/constants';
+import { action } from '@ember/object';
+
+function createDateTimeLocalString(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
 
 export default class AdminTepacheGameSessionFormComponent extends Component {
   @tracked
@@ -24,23 +35,29 @@ export default class AdminTepacheGameSessionFormComponent extends Component {
       description: this.args.gameSessionModel.description,
       logo: this.args.gameSessionModel.logo,
       gameUrn: this.args.gameSessionModel.gameUrn,
-      createdAt: new Date(
-        createdAt.getTime() - createdAt.getTimezoneOffset() * 60000
-      )
-        .toISOString()
-        .slice(0, -1),
-      expiresAt: new Date(
-        expiresAt.getTime() - expiresAt.getTimezoneOffset() * 60000
-      )
-        .toISOString()
-        .slice(0, -1),
+      createdAt: createDateTimeLocalString(createdAt),
+      expiresAt: createDateTimeLocalString(expiresAt),
     };
   }
 
   get gameSessionStateOptions() {
-    return Object.values(GAME_SESSION_STATE).map((state) => ({
-      label: state.charAt(0).toUpperCase() + state.slice(1).toLowerCase(),
-      value: state,
-    }));
+    return Object.values(GAME_SESSION_STATE);
+  }
+
+  @action
+  async saveGameSession() {
+    this.args.gameSessionModel.state = this.intermediateModel.state;
+    this.args.gameSessionModel.name = this.intermediateModel.name;
+    this.args.gameSessionModel.description = this.intermediateModel.description;
+    this.args.gameSessionModel.logo = this.intermediateModel.logo;
+    this.args.gameSessionModel.gameUrn = this.intermediateModel.gameUrn;
+    this.args.gameSessionModel.createdAt = new Date(
+      this.intermediateModel.createdAt
+    );
+    this.args.gameSessionModel.expiresAt = new Date(
+      this.intermediateModel.expiresAt
+    );
+
+    await this.args.gameSessionModel.save();
   }
 }
