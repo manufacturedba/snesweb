da3767def8192a9abe5edb1c0d626c5562d07c8a
Roberto Rodriguez <manufacturedba@gmail.com>
2022-11-30 01:12:42 -0800

    Support navigation of calendar

--- app/components/tepache-game-session-calendar.js
+++ app/components/tepache-game-session-calendar.js
@@ -1,10 +1,19 @@
 import Component from '@glimmer/component';
+import { localCopy } from 'tracked-toolbox';
+import { action } from '@ember/object';
 
 export default class TepacheGameSessionCalendarComponent extends Component {
+  @localCopy('args.gameSessionModel.createdAt') center;
+
   get range() {
     return {
       start: this.args.gameSessionModel.createdAt,
       end: this.args.gameSessionModel.expiresAt,
     };
+  }
+
+  @action
+  onCenterChange({ date }) {
+    this.center = date;
   }
 }
