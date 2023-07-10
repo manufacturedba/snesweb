1d96ed506b9391696df43872af0fae01fd8bd6d8
Roberto Rodriguez <manufacturedba@gmail.com>
2023-07-09 22:29:12 -0700

    Touch ups on name submission

--- app/components/tepache-player-enter-form.js
+++ app/components/tepache-player-enter-form.js
@@ -2,6 +2,10 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
 import { getAnalytics, logEvent } from 'firebase/analytics';
+import {
+  validatePresence,
+  validateLength,
+} from 'ember-changeset-validations/validators';
 
 function randomExtension() {
   return Math.round(Math.random() * 10000);
@@ -18,12 +22,21 @@ export default class TepachePlayerEnterFormComponent extends Component {
   async submitToEnterRoom() {
     logEvent(getAnalytics(), 'submit_player_enter_form');
 
-    if (!this.playerSessionModel.name) {
-      logEvent(getAnalytics(), 'submit_player_enter_form_anonymous');
-      this.playerSessionModel.name = `Anonymous${randomExtension()}`;
-    } else {
-      logEvent(getAnalytics(), 'submit_player_enter_form_named');
+    logEvent(getAnalytics(), 'submit_player_enter_form_named');
+
+    if (this.playerSessionModel.hasDirtyAttributes) {
+      await this.playerSessionModel.save();
     }
+
+    this.router.transitionTo('authenticated.base.play.live');
+  }
+
+  @action
+  async goAnonymous() {
+    logEvent(getAnalytics(), 'submit_player_enter_form');
+
+    logEvent(getAnalytics(), 'submit_player_enter_form_anonymous');
+    this.playerSessionModel.name = this.randomName;
 
     if (this.playerSessionModel.hasDirtyAttributes) {
       await this.playerSessionModel.save();
@@ -37,5 +50,15 @@ export default class TepachePlayerEnterFormComponent extends Component {
    */
   get playerSessionModel() {
     return this.args.playerSessionModel;
+  }
+
+  get playerSessionValidations() {
+    return {
+      name: [validatePresence(true), validateLength({ min: 1, max: 36 })],
+    };
+  }
+
+  get randomName() {
+    return `Anonymous${randomExtension()}`;
   }
 }
