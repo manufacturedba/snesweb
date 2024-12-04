9872ad112c2459d08cc919ec42f67ac6e0e59f50
Roberto Rodriguez <manufacturedba@gmail.com>
2024-12-03 21:20:51 -0800

    Take in a string

--- app/helpers/string-to-random-color.js
+++ app/helpers/string-to-random-color.js
@@ -1,10 +1,15 @@
 import { helper } from '@ember/component/helper';
 
-function generateBrightHexColor() {
-  const getBrightValue = () => Math.floor(Math.random() * 156) + 100;
-  const red = getBrightValue();
-  const green = getBrightValue();
-  const blue = getBrightValue();
+function generateBrightHexColor(inputString) {
+  let hash = 0;
+  for (let i = 0; i < inputString.length; i++) {
+    hash = inputString.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  const getBrightValue = (value) => (value % 156) + 100;
+  const red = getBrightValue(hash & 0xff);
+  const green = getBrightValue((hash >> 8) & 0xff);
+  const blue = getBrightValue((hash >> 16) & 0xff);
 
   const toHex = (value) => value.toString(16).padStart(2, '0');
   const hexColor = `#${toHex(red)}${toHex(green)}${toHex(blue)}`;
